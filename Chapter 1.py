6f2f09843fc7681ac6b055adc5928b23efda8113
Kyle McGourty <kyle.mcgourty@gmail.com>
2018-10-24 11:12:33 -0700

    begin chapter 1

---
+++ Chapter 1.py
@@ -0,0 +1,50 @@
+
+#Chapter 1
+
+
+#1.1
+
+answer = "N!"
+
+#1.2
+
+"""When s0 = 0"""
+s1 = 3
+s1 = 6
+s2 = 9
+s3 = 12
+
+"""When s0 = 4"""
+
+s1 = 7
+s2 = 10
+s3 = 13
+s4 = 16
+
+answer = "Both cases are an arithmetic sequence where r = 3."
+
+
+#1.3
+
+
+answer = "If given si and sj, a series of linear equations can be created. These can be solved for the intermediary" \
+         "values of s." \
+
+s2 = 1 + s0
+
+s3 = (1+s0) + 1
+
+s4 = [(1+s0) + 1] + [1 + s0]
+
+s5 = 17 = [(1+s0) + 1] + [1 + s0] + [(1+s0) + 1]
+
+17 = 3s0 + 5
+
+s0 = 4
+
+
+#1.4
+
+answer = "D(P) = { 0                if C(P) == 0" \
+         "         C(P) + D(C(P))   if C(P) > 0 "
+
